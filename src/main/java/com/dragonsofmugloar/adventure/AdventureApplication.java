362d442fa1126ecdc3aa6f940b5c230af67f3003
Rajan Raj Das <dasrajanraj@gmail.com>
2025-07-27 12:49:27 +0300

    starter for user input

--- src/main/java/com/dragonsofmugloar/adventure/AdventureApplication.java
+++ src/main/java/com/dragonsofmugloar/adventure/AdventureApplication.java
@@ -1,13 +1,31 @@
 package com.dragonsofmugloar.adventure;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdventureApplication {
+public class AdventureApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdventureApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureApplication.class, args);
+    }
 
+	@Override
+    public void run(String... args) {
+        System.out.println("How many Dragon of Mugloar games do you want to start?");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+			if (scanner.hasNextInt()) {
+                int numberOfDragons = scanner.nextInt();
+                System.out.println("You have chosen to play " + numberOfDragons + " games today!");
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
